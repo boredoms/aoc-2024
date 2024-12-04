4ae3e909044452f47ba746178637c40d3a589c24
max <git@voets.ch>
2024-12-04 13:30:25 +0100

    update template so it compiles right away

--- src/template/template.rs
+++ src/template/template.rs
@@ -1,9 +1,11 @@
 pub fn solve_part_one(input: &str) -> usize {
     // solution goes here
+    todo!()
 }
 
 pub fn solve_part_two(input: &str) -> usize {
     // solution goes here
+    todo!()
 }
 
 #[cfg(test)]
@@ -12,13 +14,13 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        let result = solve_part_one();
+        let result = solve_part_one("");
         assert_eq!(0, result);
     }
 
     #[test]
     fn test_part_two() {
-        let result = solve_part_two();
+        let result = solve_part_two("");
         assert_eq!(0, result);
     }
 }
