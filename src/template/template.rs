8e47a688f71fda8532ddbe503c838a2c38c3f0ce
max <git@voets.ch>
2025-01-19 22:07:57 +0100

    update template

--- src/template/template.rs
+++ src/template/template.rs
@@ -1,8 +1,12 @@
-pub fn solve_part_one(input: &str) -> usize {
+type Input = todo!();
+
+pub fn parse(input: &str) -> Input {}
+
+pub fn solve_part_one(input: &Input) -> usize {
     todo!()
 }
 
-pub fn solve_part_two(input: &str) -> usize {
+pub fn solve_part_two(input: &Input) -> usize {
     todo!()
 }
 
@@ -10,15 +14,25 @@ pub fn solve_part_two(input: &str) -> usize {
 mod tests {
     use super::*;
 
+    static TEST_DATA_PATH: &str = "data/test/year20XX/dayXX.txt";
+
     #[test]
     fn test_part_one() {
-        let result = solve_part_one("");
+        let input = &std::fs::read_to_string(TEST_DATA_PATH).expect("Test data does not exist.");
+
+        let input = parse(input);
+        let result = solve_part_one(&input);
+
         assert_eq!(0, result);
     }
 
     #[test]
     fn test_part_two() {
-        let result = solve_part_two("");
+        let input = &std::fs::read_to_string(TEST_DATA_PATH).expect("Test data does not exist.");
+
+        let input = parse(input);
+        let result = solve_part_two(&input);
+
         assert_eq!(0, result);
     }
 }
