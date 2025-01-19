a4f5c6736fce95b94b98b37928c5a381da0ce7cb
max <git@voets.ch>
2025-01-19 21:15:47 +0100

    add benchmarking macro

--- benches/all.rs
+++ benches/all.rs
@@ -1,53 +1,41 @@
-use criterion::{criterion_group, criterion_main, Criterion};
-use rust_aoc::solvers::{day1, day2, day3, day4, day5};
-use std::hint::black_box;
+use criterion::criterion_main;
+// this macro generates a module containing the benchmarks for a single year
 
-fn criterion_benchmark(c: &mut Criterion) {
-    // let input = std::fs::read_to_string("data/day1/input.txt").unwrap();
+macro_rules! benchmark_year {
+    ($year:tt; $($day:tt),*) => {
+        mod $year {
+            use criterion::{criterion_group, Criterion};
+            use std::hint::black_box;
 
-    // c.bench_function("Day 1, Part 1", |b| {
-    //     b.iter(|| day1::solve_part_one(black_box(&input)))
-    // });
-    // c.bench_function("Day 1, Part 2", |b| {
-    //     b.iter(|| day1::solve_part_two(black_box(&input)))
-    // });
+            $(
+            pub fn $day(c: &mut Criterion) {
+                static DAY: &str = stringify!($day);
 
-    // let input = std::fs::read_to_string("data/day2/input.txt").unwrap();
+                use std::fs::read_to_string;
+                use rust_aoc::solvers::$day::{parse, solve_part_one, solve_part_two};
 
-    // c.bench_function("Day 2, Part 1", |b| {
-    //     b.iter(|| day2::solve_part_one(black_box(&input)))
-    // });
-    // c.bench_function("Day 2, Part 2", |b| {
-    //     b.iter(|| day2::solve_part_two(black_box(&input)))
-    // });
+                let path = "data/".to_owned() + stringify!($day) + "/input.txt";
 
-    // let input = std::fs::read_to_string("data/day3/input.txt").unwrap();
+                let input = read_to_string(path).unwrap();
 
-    // c.bench_function("Day 3, Part 1", |b| {
-    //     b.iter(|| day3::solve_part_one(black_box(&input)))
-    // });
-    // c.bench_function("Day 3, Part 2", |b| {
-    //     b.iter(|| day3::solve_part_two(black_box(&input)))
-    // });
+                c.bench_function(&format!("{}: Parse", DAY), |b| {
+                    b.iter(|| parse(black_box(&input)))
+                });
 
-    // let input = std::fs::read_to_string("data/day4/input.txt").unwrap();
+                c.bench_function(&format!("{}: Part 1", DAY), |b| {
+                    b.iter(|| solve_part_one(black_box(&input)))
+                });
 
-    // c.bench_function("Day 4, Part 1", |b| {
-    //     b.iter(|| day4::solve_part_one(black_box(&input)))
-    // });
-    // c.bench_function("Day 4, Part 2", |b| {
-    //     b.iter(|| day4::solve_part_two(black_box(&input)))
-    // });
+                c.bench_function(&format!("{}: Part 2", DAY), |b| {
+                    b.iter(|| solve_part_two(black_box(&input)))
+                });
+            })*
 
-    let input = std::fs::read_to_string("data/day5/input.txt").unwrap();
-
-    c.bench_function("Day 5, Part 1", |b| {
-        b.iter(|| day5::solve_part_one(black_box(&input)))
-    });
-    c.bench_function("Day 5, Part 2", |b| {
-        b.iter(|| day5::solve_part_two(black_box(&input)))
-    });
+            criterion_group!(benches, $($day, )*);
+        }
+    };
 }
 
-criterion_group!(benches, criterion_benchmark);
-criterion_main!(benches);
+benchmark_year!(year2024; day01, day02);
+
+criterion_main!(year2024::benches);
