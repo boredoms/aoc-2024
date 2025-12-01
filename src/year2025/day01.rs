a535a1cf725eb9f9017ba8c6c0817e29cbf6a699
boredoms <max@voets.ch>
2025-12-01 20:24:17 +0100

    feat(2025): solve day 1

--- src/year2025/day01.rs
+++ src/year2025/day01.rs
@@ -1,20 +1,67 @@
-type Input = todo!();
+const DIAL_START: i32 = 50;
 
-pub fn parse(input: &str) -> Input {}
+type Input = Vec<i32>;
+
+pub fn parse(input: &str) -> Input {
+    input
+        .lines()
+        .map(|line| {
+            let n = line
+                .chars()
+                .skip(1)
+                .fold(0, |n, c| n * 10 + c.to_digit(10).unwrap()) as i32;
+            if line.starts_with('L') {
+                -n
+            } else {
+                n
+            }
+        })
+        .collect()
+}
 
 pub fn solve_part_one(input: &Input) -> usize {
-    todo!()
+    input
+        .iter()
+        .fold((0, DIAL_START), |(key, sum), n| {
+            (
+                key + if (sum + n) % 100 == 0 { 1 } else { 0 },
+                (sum + n) % 100,
+            )
+        })
+        .0 as usize
 }
 
 pub fn solve_part_two(input: &Input) -> usize {
-    todo!()
+    input
+        .iter()
+        .fold((0, DIAL_START), |(key, sum), n| {
+            let crossings = if *n < 0 {
+                (n + (sum - 100) % 100) / 100
+            } else {
+                (n + (sum + 100) % 100) / 100
+            };
+            (key + crossings.abs(), (sum + n) % 100)
+        })
+        .0 as usize
+}
+
+pub fn solve(filename: &str) -> Result<(String, String), String> {
+    let input =
+        &std::fs::read_to_string(filename).or(Err(format!("could not read file {}", filename)))?;
+
+    let input = parse(input);
+
+    Ok((
+        solve_part_one(&input).to_string(),
+        solve_part_two(&input).to_string(),
+    ))
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    static TEST_DATA_PATH: &str = "data/test/year20XX/dayXX.txt";
+    static TEST_DATA_PATH: &str = "data/test/year2025/day01.txt";
 
     #[test]
     fn test_part_one() {
@@ -23,7 +70,7 @@ mod tests {
         let input = parse(input);
         let result = solve_part_one(&input);
 
-        assert_eq!(0, result);
+        assert_eq!(3, result);
     }
 
     #[test]
@@ -33,6 +80,6 @@ mod tests {
         let input = parse(input);
         let result = solve_part_two(&input);
 
-        assert_eq!(0, result);
+        assert_eq!(6, result);
     }
 }
